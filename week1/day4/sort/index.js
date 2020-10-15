5089a8b7ac6ca25d5c5538f390ba1131ea5e7397
jossdz <carloscorrea.dev@gmail.com>
2020-10-15 11:25:43 -0500

    w1d4p1

--- week1/day4/sort/index.js
+++ week1/day4/sort/index.js
@@ -6,6 +6,10 @@ console.log('sort')
 
 const numbers = [22, 23, 99, 68, 1, 0, 9, 112, 223, 64, 18];
 
+// const orderedNumbers = [...numbers].sort((a, b) => {
+//   return a - b
+// })
+
 const orderedNumbers = numbers.map(n=>n).sort((a, b) => {
   return a - b
 })
