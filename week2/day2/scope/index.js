b8c4b62a5b3aa151fde1e8d246b2cd544427a447
jossdz <carloscorrea.dev@gmail.com>
2020-10-20 16:34:51 -0500

    w2d2p2

---
+++ week2/day2/scope/index.js
@@ -0,0 +1,38 @@
+"use strict"
+// console.log('hola');
+// let x = 'x'
+
+// function sum() {
+//   let y = 'y'
+//   console.log(x, y)
+// }
+// // console.log(y)
+// sum()
+// let i = 1
+
+// console.log(i)
+
+// for (let i = 1; i <= 30; i++) {
+//   console.log(`Iteration number: ${i}`);
+// }
+
+// console.log(i)
+
+
+// console.log(a)
+// var a = 'a'
+// console.log(a)
+
+// let a = 1;
+// let b = 2;
+ 
+// function inner() {
+//   a = 4; // reassigned
+//   let b = 3; // declared in an inner scope
+// }
+ 
+// inner();
+// console.log(a); // => 4
+// console.log(b); // => 2
+
+x = 's'
