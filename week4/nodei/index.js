c36235970d2898bf04421db261cb09e9f40d8114
jossdz <carloscorrea.dev@gmail.com>
2020-11-02 11:57:04 -0600

    node

---
+++ week4/nodei/index.js
@@ -0,0 +1,8 @@
+const chalk = require("chalk")
+const chalkAnimation = require("chalk-animation")
+const obj = require("./obj")
+const cows = require("cows")
+var cats = require("cat-ascii-faces")
+
+// chalkAnimation.rainbow(cows()[0])
+chalkAnimation.rainbow(cats())
