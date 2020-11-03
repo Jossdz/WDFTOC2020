eb8dce8057fd2d3a5500d69cf9727cdbc3d4a47b
jossdz <carloscorrea.dev@gmail.com>
2020-11-03 17:28:20 -0600

    expresssssssss

---
+++ week4/day2/expressi/index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+//con esta linea le decimos a express que sirva de forma publica los archivos de /public
+app.use(express.static("public"))
+
+app.get("/", (request, response) => {
+  response.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.get("/cat", (req, res) => {
+  res.sendFile(`${__dirname}/views/cat.html`)
+})
+
+app.listen(3000)
