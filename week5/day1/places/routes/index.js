aecd6d6a017a93a0c9c496a50704b6f2e7b36fe7
jossdz <carloscorrea.dev@gmail.com>
2020-11-10 15:26:44 -0600

    maps

---
+++ week5/day1/places/routes/index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const router = express.Router()
+const {
+  newPlaceForm,
+  addNewPlace,
+  detailPlaceView
+} = require("../controllers/places")
+const Place = require("../models/Place")
+/* GET home page */
+router.get("/", async (req, res, next) => {
+  const places = await Place.find()
+  res.render("index", { token: process.env.MAPBOX_TOKEN, places })
+})
+
+router.get("/place/add", newPlaceForm)
+router.post("/place/add", addNewPlace)
+router.get("/place/:placeId", detailPlaceView)
+
+module.exports = router
